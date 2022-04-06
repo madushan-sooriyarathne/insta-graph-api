05d0248aff8116b5c7aa6dc2a36b9cfd02d02033
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2022-04-06 09:25:27 +0530

    added login scopes and logout button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const handleClick = () => {
+  const handleLogin = () => {
     FB.getLoginStatus(function (response) {
       if (response.status === "connected") {
         document.body.append(
@@ -13,23 +11,33 @@ const Home: NextPage = () => {
           ))
         );
       } else {
-        FB.login(function (response) {
-          if (response.authResponse) {
-            console.log("Welcome!  Fetching your information.... ");
-            FB.api("/me", {}, function (response: fb.User) {
-              console.log("Good to see you, " + response.name + ".");
-            });
-          } else {
-            console.log("User cancelled login or did not fully authorize.");
-          }
-        });
+        FB.login(
+          function (response) {
+            if (response.authResponse) {
+              console.log("Welcome!  Fetching your information.... ");
+              FB.api("/me", {}, function (response: fb.User) {
+                console.log("Good to see you, " + response.name + ".");
+              });
+            } else {
+              console.log("User cancelled login or did not fully authorize.");
+            }
+          },
+          { scope: "public_profile, instagram_basic, pages_show_list" }
+        );
       }
+    });
+  };
+
+  const handleLogout = () => {
+    FB.logout((response) => {
+      alert(response.status);
     });
   };
 
   return (
     <div className={styles.container}>
-      <button onClick={handleClick}>Share on FB</button>
+      <button onClick={handleLogin}>Login to Facebook</button>
+      <button onClick={handleLogout}>Logout from Facebook</button>
     </div>
   );
 };
