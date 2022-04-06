ea133cd33c0d43ac6452abea1b2ecb519f4fe666
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2022-04-06 09:11:25 +0530

    added login status check

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,24 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const handleClick = () => {
-    FB.login(function (response) {
-      if (response.authResponse) {
-        console.log("Welcome!  Fetching your information.... ");
-        FB.api("/me", {}, function (response: fb.User) {
-          console.log("Good to see you, " + response.name + ".");
-        });
+    FB.getLoginStatus(function (response) {
+      if (response.status === "connected") {
+        document.body.append(
+          (document.createElement("p").textContent = JSON.stringify(
+            response.authResponse
+          ))
+        );
       } else {
-        console.log("User cancelled login or did not fully authorize.");
+        FB.login(function (response) {
+          if (response.authResponse) {
+            console.log("Welcome!  Fetching your information.... ");
+            FB.api("/me", {}, function (response: fb.User) {
+              console.log("Good to see you, " + response.name + ".");
+            });
+          } else {
+            console.log("User cancelled login or did not fully authorize.");
+          }
+        });
       }
     });
   };
